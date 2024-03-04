98cbfc1f46e58fd3e9941fb4bb3acdf8e6095694
Yanter44 <zhmih44@gmail.com>
2024-03-04 21:39:11 +0300

    -shit

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LerningAsp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Registr()
+        {
+            return View("Registr");
+        }
+        public IActionResult Loging()
+        {
+            return View();
+        }
+    }
+}
